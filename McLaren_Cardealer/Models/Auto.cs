9f4dc534233faed49ef83907dc5164611e68ece8
stijn <stijn@MSI>
2022-10-24 14:39:16 +0200

    Add project files.

---
+++ McLaren_Cardealer/Models/Auto.cs
@@ -0,0 +1,16 @@
+﻿namespace McLaren_Cardealer.Models
+
+{
+    public class Auto
+    {
+        public int AutoId { get; set; }
+        public string Naam { get; set; }
+        public int Prijs { get; set; }
+        public string PK { get; set; }
+        public string Kilogram { get; set; }
+        public string Motor { get; set; }
+        public string Torque { get; set; }
+        public string Kleur { get; set; }
+
+    }
+}
---
+++ McLaren_Cardealer/Models/Klant.cs
@@ -0,0 +1,11 @@
+﻿namespace McLaren_Cardealer.Models
+{
+    public class Klant
+    {
+        public int KlantId { get; set; }
+        public string Naam { get; set; }
+        public string Voornaam { get; set; }
+        public string Gemeente { get; set; }
+        public string Rekeningnummer { get; set; }
+    }
+}
