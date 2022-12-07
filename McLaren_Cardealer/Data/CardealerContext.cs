3471994f5a67873f6c9da514415638789c240c9e
StijnVanHees <r0887424@student.thomasmore.be>
2022-12-07 09:10:21 +0100

    07-12-22

---
+++ McLaren_Cardealer/Data/CardealerContext.cs
@@ -0,0 +1,17 @@
+﻿using McLaren_Cardealer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace McLaren_Cardealer.Data
+{
+    public class CardealerContext 
+    {
+
+        public DbSet<Auto> Autos { get; set; }
+        public DbSet<Factuur> Facturen { get; set; }
+        public DbSet<Klant> Klanten { get; set; }
+        public DbSet<Motor> Motoren { get; set; }
+        public DbSet<Wielen> Wielen { get; set; }
+
+
+    }
+}
---
+++ McLaren_Cardealer/ViewModels/WinkelViewModel.cs
@@ -0,0 +1,10 @@
+﻿using McLaren_Cardealer.Models;
+using System.Collections.Generic;
+
+namespace McLaren_Cardealer.ViewModels
+{
+    public class WinkelViewModel
+    {
+        public List<Auto> Autos { get; set; }
+    }
+}
